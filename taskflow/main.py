0e77ddbcb7bac41c1ed086cbf941a69b54e7ffce
SemenIvanovich96 <SaiRex15@yandex.ru>
2026-05-25 11:44:36 +0300

    add template login

--- taskflow/main.py
+++ taskflow/main.py
@@ -7,6 +7,7 @@ from src.routers import router as api_router
 from src.utils.loguru_config import AppLogger
 from src.routers.pages.index import router as index_router
 from fastapi.staticfiles import StaticFiles
+from src.routers.pages.login import router as login_pages_router
 
 logger = AppLogger().get_logger()
 
@@ -15,6 +16,7 @@ main_app = fastapi_create_app()
 main_app.mount("/images", StaticFiles(directory="src/images"), name="images")
 main_app.include_router(router=api_router, prefix=settings.api.PREFIX)
 main_app.include_router(router=index_router)
+main_app.include_router(login_pages_router)
 main_app.add_middleware(SessionMiddleware, secret_key=settings.run.SECRET_KEY)
 
 if __name__ == "__main__":
